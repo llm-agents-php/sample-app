f851a86ee782c0e3874547af0ac3df25a64572c9
Pavel Buchnev <butschster@gmail.com>
2024-09-04 22:20:13 +0400

    Adds prompt generator with interceptors

--- app/src/Application/Bootloader/AgentsChatBootloader.php
+++ app/src/Application/Bootloader/AgentsChatBootloader.php
@@ -6,10 +6,14 @@ namespace App\Application\Bootloader;
 
 use App\Domain\Chat\SimpleChatService;
 use App\Infrastructure\RoadRunner\Chat\ChatHistoryRepository;
-use LLM\Agents\Chat\AgentPromptGenerator;
 use LLM\Agents\Chat\ChatHistoryRepositoryInterface;
 use LLM\Agents\Chat\ChatServiceInterface;
-use LLM\Agents\LLM\AgentPromptGeneratorInterface;
+use LLM\Agents\PromptGenerator\Interceptors\AgentMemoryInjector;
+use LLM\Agents\PromptGenerator\Interceptors\InstructionGenerator;
+use LLM\Agents\PromptGenerator\Interceptors\LinkedAgentsInjector;
+use LLM\Agents\PromptGenerator\Interceptors\SessionContextInjector;
+use LLM\Agents\PromptGenerator\Interceptors\UserPromptInjector;
+use LLM\Agents\PromptGenerator\PromptGeneratorPipeline;
 use Spiral\Boot\Bootloader\Bootloader;
 use Spiral\Cache\CacheStorageProviderInterface;
 
@@ -19,12 +23,25 @@ final class AgentsChatBootloader extends Bootloader
     {
         return [
             ChatServiceInterface::class => SimpleChatService::class,
-            AgentPromptGeneratorInterface::class => AgentPromptGenerator::class,
             ChatHistoryRepositoryInterface::class => static fn(
                 CacheStorageProviderInterface $cache,
             ): ChatHistoryRepositoryInterface => new ChatHistoryRepository(
                 $cache->storage('chat-messages'),
             ),
+
+            PromptGeneratorPipeline::class => static function (
+                LinkedAgentsInjector $linkedAgentsInjector,
+            ): PromptGeneratorPipeline {
+                $pipeline = new PromptGeneratorPipeline();
+
+                return $pipeline->withInterceptor(
+                    new InstructionGenerator(),
+                    new AgentMemoryInjector(),
+                    $linkedAgentsInjector,
+                    new SessionContextInjector(),
+                    new UserPromptInjector(),
+                );
+            },
         ];
     }
 }
